839852a298707d28a6f925f8a736bac95faec384
arsen-hdm <arsenh90@gmail.com>
2025-02-12 22:17:45 +0100

    Add files via upload

---
+++ arm_control/launch/arm_control.launch.py
@@ -0,0 +1,52 @@
+import os
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from ament_index_python.packages import get_package_share_directory
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import (
+    DeclareLaunchArgument,
+    IncludeLaunchDescription,
+)
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+
+def generate_launch_description():
+
+    joint_state_broadcaster = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+    )  
+
+    position_controller = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["position_controller", "--controller-manager", "/controller_manager"],  
+    ) 
+    
+    delay_joint_pos_controller = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=gz_spawn_entity,
+            on_exit=[position_controller],
+        )
+    )
+    
+    delay_joint_state_broadcaster = (
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=gz_spawn_entity,
+                on_exit=[joint_state_broadcaster],
+            )
+        )
+    )
+    
+    nodes_to_start = [
+       delay_joint_pos_controller, 
+       delay_joint_state_broadcaster,
+    ]
+
+    return LaunchDescription(nodes_to_start) 
